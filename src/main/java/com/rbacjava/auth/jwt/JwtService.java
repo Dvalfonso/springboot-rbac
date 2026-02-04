05fb6566048d122ba3f2fb323b8da7af13d069d6
David <davidalfonsook@gmail.com>
2026-02-03 21:22:36 -0300

    Implement auth with jwt

--- src/main/java/com/rbacjava/auth/jwt/JwtService.java
+++ src/main/java/com/rbacjava/auth/jwt/JwtService.java
@@ -3,6 +3,7 @@ package com.rbacjava.auth.jwt;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -11,20 +12,21 @@ import java.util.Date;
 @Service
 public class JwtService {
 
-    private static final String SECRET = "super-secret-key-super-secret-key";
+    @Value("${jwt.secret}")
+    private String jwtSecret;
 
     public String generateToken(UserDetails user) {
         return Jwts.builder()
                 .setSubject(user.getUsername())
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
-                .signWith(Keys.hmacShaKeyFor(SECRET.getBytes()), SignatureAlgorithm.HS256)
+                .signWith(Keys.hmacShaKeyFor(jwtSecret.getBytes()), SignatureAlgorithm.HS256)
                 .compact();
     }
 
     public String extractUsername(String token) {
         return Jwts.parser()
-                .setSigningKey(SECRET.getBytes())
+                .setSigningKey(jwtSecret.getBytes())
                 .build()
                 .parseClaimsJws(token)
                 .getBody()
